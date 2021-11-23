2cd3765b85358d6901c8b01f85fccad35f5ae41f
Marcelo Teixeira dos Santos <marcelo.teixeira@cern.ch>
2021-11-23 09:02:19 +0100

    Leverage static generation for podcast pages

--- client/src/pages/podcasts/[id].js
+++ client/src/pages/podcasts/[id].js
@@ -1,5 +1,4 @@
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
 
 import strapiApi from '../../services/strapi';
 
@@ -7,19 +6,9 @@ import styles from '../../styles/PodcastView.module.css';
 
 import { EpisodeCard } from '../../components/EpisodeCard';
 
-export default function PodcastView() {
+export default function PodcastView({ podcast }) {
   const router = useRouter();
-  const { query: { id } } = router;
-
-  const [podcast, setPodcast] = useState(null);
-
-  useEffect(async () => {
-    if (id === undefined) return;
-
-    const response = await strapiApi.get(`/podcasts/${id}`);
-
-    setPodcast(response?.data);
-  }, [id]);
+  const { query: { id: podcastId } } = router;
 
   const redirectToHomepage = () => router.push("/");
 
@@ -33,53 +22,75 @@ export default function PodcastView() {
         await strapiApi.delete(`/episodes/${episode?.id}`);
       });
 
-      await strapiApi.delete(`/podcasts/${id}`);
+      await strapiApi.delete(`/podcasts/${podcastId}`);
 
       redirectToHomepage();
     }
   }
 
   return (
-    <div className={styles.podcastviewcontainer}>
-      <div className={styles.podcastviewmain}>
-        <div
-          className={styles.podcastviewimg}
-          style={{ backgroundImage: `url(${podcast?.imageUrl})` }}
-        ></div>
-        <div style={{ width: "100%" }}>
-          <div className={styles.podcastviewname}>
-            <h1>{podcast?.name}</h1>
-          </div>
-
-          <div className={styles.podcastviewminidet}>
-            <div>
-              <span style={{ marginRight: "4px", color: "rgb(142 142 142)" }}>
-                Created by:
-              </span>
-              <span style={{ fontWeight: "600" }}>{podcast?.author}</span>
-            </div>
-            <div style={{ padding: "14px 0" }}>
-              <span>
-                <button onClick={deletePodcast} className="btn-danger">
-                  Delete
-                </button>
-              </span>
-            </div>
-          </div>
-
-          <div className={styles.podcastviewepisodescont}>
-            <div className={styles.podcastviewepisodes}>
-              <h2>Episodes</h2>
+    <div style={{ margin: "0 auto", maxWidth: "1480px" }} >
+      <div className={styles.podcastviewcontainer}>
+        <div className={styles.podcastviewmain}>
+          <div id="logo"
+            className={styles.podcastviewimg}
+            style={{ backgroundImage: `url(${podcast?.imageUrl})` }}
+          ></div>
+          <div style={{ width: "100%" }}>
+            <div className={styles.podcastviewname}>
+              <h1>{podcast?.name}</h1>
             </div>
 
-            <div className={styles.podcastviewepisodeslist}>
-              {podcast?.episodes.map((episode, i) => (
-                <EpisodeCard key={i} episode={episode} defaultImage={podcast.imageUrl}/>
-              ))}
+            <div className={styles.podcastviewminidet}>
+              <div>
+                <span style={{ marginRight: "4px", color: "rgb(142 142 142)" }}>
+                  Created by:
+                </span>
+                <span style={{ fontWeight: "600" }}>{podcast?.author}</span>
+              </div>
+              <div style={{ padding: "14px 0" }}>
+                <span>
+                  <button onClick={deletePodcast} className="btn-danger">
+                    Delete
+                  </button>
+                </span>
+              </div>
+            </div>
+
+            <div className={styles.podcastviewepisodescont}>
+                <div className={styles.podcastviewepisodes}>
+                  <h2>Episodes</h2>
+                </div>
+
+                <div className={styles.podcastviewepisodeslist}>
+                  {podcast?.episodes.map((episode, i) => (
+                    <EpisodeCard key={i} episode={episode} defaultImage={podcast.imageUrl}/>
+                  ))}
+                </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      </div>
   );
+}
+
+export async function getStaticProps({ params }) {
+  const { data: podcast } = await strapiApi.get(`/podcasts/${params.id}`);
+
+  return {
+    props: {
+      podcast,
+    },
+  }
+}
+
+export async function getStaticPaths() {
+  const { data: podcasts } = await strapiApi.get("/podcasts");
+
+  const paths = podcasts.map((podcast) => ({
+    params: { id: podcast.id.toString() },
+  }));
+
+  return { paths, fallback: 'blocking' };
 }
